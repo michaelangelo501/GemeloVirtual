50046748800c73f80c2f6418b5802bcc6b70b71b
miguel angel herrera sanchez <sanchezherreraangelmiguel@gmail.com>
2022-11-14 11:32:09 -0600

    Se creo la Base de Datos, se creo las clases de User y Patient Se modificaron el json,  DataContext y StartUp

---
+++ GemeloVirtual.Web/Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using GemeloVirtual.Web.Data.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace GemeloVirtual.Web.Data
+{
+    public class DataContext : IdentityDbContext<User>
+    {
+        public DbSet<Patient> Patients { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ GemeloVirtual.Web/Data/Entities/Patient.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GemeloVirtual.Web.Data.Entities
+{
+    public class Patient
+    {
+        public int Id { get; set; }
+
+        public User User { get; set; }
+
+        public DateTime Birthday { get; set; }
+    }
+}
